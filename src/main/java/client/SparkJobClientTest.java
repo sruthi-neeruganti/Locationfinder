29e02250129e8365466c7fa56df8a75328822f7b
sruthi-neeruganti <sruthi.neeruganti@gmail.com>
2020-11-18 05:48:57 -0800

    files are push to spring-boot branch

---
+++ src/main/java/client/SparkJobClientTest.java
@@ -0,0 +1,42 @@
+package client;
+
+import java.util.List;
+import java.io.File;
+
+import com.bluebreezecf.tools.sparkjobserver.api.ISparkJobServerClient;
+import com.bluebreezecf.tools.sparkjobserver.api.SparkJobJarInfo;
+import com.bluebreezecf.tools.sparkjobserver.api.SparkJobServerClientException;
+import com.bluebreezecf.tools.sparkjobserver.api.SparkJobServerClientFactory;
+
+public class SparkJobClientTest {
+
+	public static void main(String[] args) {
+		ISparkJobServerClient client = null;
+		try {
+			client = SparkJobServerClientFactory.getInstance().createSparkJobServerClient("http://localhost:4567/");
+
+			List<SparkJobJarInfo> jarInfos = client.getJars();
+			for (SparkJobJarInfo jarInfo : jarInfos) {
+				System.out.println(jarInfo.toString());
+			}
+
+			// POST /jars/<appName>
+			client.uploadSparkJobJar(
+					new File("/Users/guruboya/Desktop/myapp-0.0.1-SNAPSHOT.jar"), null);
+
+			// GET /contexts
+			List<String> contexts = client.getContexts();
+			System.out.println("Current contexts:");
+			for (String cxt : contexts) {
+				System.out.println(cxt);
+			}
+			//client.createContext("hello",null);
+
+		} catch (SparkJobServerClientException e1) {
+			e1.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/ipgeolocation/GeoLocationController.java
@@ -0,0 +1,54 @@
+package com.ipgeolocation;
+
+import static spark.Spark.get;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+//import com.spark.test.GeoLocationData;
+
+import io.ipgeolocation.api.Geolocation;
+import io.ipgeolocation.api.GeolocationParams;
+import io.ipgeolocation.api.IPGeolocationAPI;
+
+@RestController
+@RequestMapping("/geolocate")
+public class GeoLocationController {
+
+	/**
+	 * 
+	 * @param ipaddress
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/{ipaddress}")
+	//Rest api to get location data based on ip address
+	public GeoLocationData read(@PathVariable String ipaddress) {
+		IPGeolocationAPI api = new IPGeolocationAPI("fca667c3cab84187b60b5b71d6e0bef0");
+
+		GeolocationParams geoParams = new GeolocationParams();
+		geoParams.setIPAddress(ipaddress);
+		geoParams.setFields("geo,time_zone,currency");
+
+		Geolocation geolocation = api.getGeolocation(geoParams);
+		GeoLocationData data = new GeoLocationData();
+		
+		if (geolocation.getStatus() == 200) {
+			data.setCountryName(geolocation.getCountryName());
+			data.setCurrencyName(geolocation.getCurrency().getName());
+			data.setStatusCode(geolocation.getStatus());
+			data.setMessage(geolocation.getMessage());
+
+		} else {
+			data.setStatusCode(geolocation.getStatus());
+			data.setMessage(geolocation.getMessage());
+			System.out.printf("Status Code: %d, Message: %s\n", geolocation.getStatus(), geolocation.getMessage());
+		}
+		return data;
+	}
+
+	
+
+}
